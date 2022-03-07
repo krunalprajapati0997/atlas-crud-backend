a2869b00483c15d243d56319cc2c8804ab29df14
krunalprajapati0997 <krunalprajapati22121999@gmail.com>
2022-03-07 11:30:01 +0530

    backend

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const mongoose =require('mongoose');
 const route = require('./route/routers')
 const user = require('./model/user')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app =  express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/app',route);
+app.use('/',route);
 
 const URL = 'mongodb+srv://krunalprajapati1234:krunal0997@cluster0.c9t3e.mongodb.net/CRUDAPP?retryWrites=true&w=majority'
 mongoose.connect(URL)
--- route/routers.js
+++ route/routers.js
@@ -1,12 +1,54 @@
+const { Router } = require('express');
 const express = require('express');
 const route = express.Router();
+
+
 const user = require("../model/user")
+const {ObjectId} =require('mongodb')
 
 
-route.get('/',(req,res)=>{
-    console.log('hey..R.. Get')
-    res.send('this a aaaa page')
+// route.get('/',(req,res)=>{
+//     console.log('hey..R.. Get')
+//     res.send('this a aaaa page')
+// })
+
+// route.get('/user/:id',(req,res)=>{
+//     const id = req.params.id;
+//     user.findById(id).exec((err,result)=>{
+//         if(err){
+//             console.log(err)
+//         }else{
+//             res.send(result);
+//         }
+        
+//     })
+// })
+
+route.get('/', (req, res) => {
+    user.find({}).exec(function (err, ress) {n
+        res.json({ 'data': ress })
+    })
 })
+
+route.delete('/:id', (req, res) => {
+    const id = req.params.id
+    user.findByIdAndDelete({_id}).exec(function (err, ress) {
+        res.json({ 'data': ress })
+    })
+})
+
+
+
+
+
+// route.get('/', (req, res) => {
+//         const id = req.params.id
+//         user.findById(ObjectId("622308fd8175791718898e69"))
+
+//         res.send(req.body)
+//         console.log('hey get')
+//     })
+
 
 route.post('/',async(req,res)=>{
     console.log('hey..post');
@@ -23,11 +65,11 @@ route.put('/:id',async(req,res)=>{
     res.send(req.body)
 })
 
-route.delete("/:id",async(req,res)=>{
-    const id = req.params.id
-     const user2 = await user.findByIdAndDelete(req.params.id);
+// route.delete("/:id",async(req,res)=>{
+//     const id = req.params.id
+//      const user2 = await user.findByIdAndDelete(req.params.id);
      
-     res.send(user2)
- })
+//      res.send(user2)
+//  })
 
 module.exports = route
